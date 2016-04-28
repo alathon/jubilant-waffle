3c05d84e7dd67995f44c9aa25a2bcaed2083186d
wallvizpc <wallvizpc>
2016-04-28 15:43:36 +0200

    OptiTrack support added
    - Ground redone as quad instead of plane.
    - FollowRigidbody can be attached to a GameObject and it will then
    follow a particular rigidbody coming in from OptiTrack
    - Support for OptiTrack added via github/XmanLCH/MotiveDirect
    - Ground plane mapped to Optitrack coords via GroundMapper.
    - Moved scene around a bit.

---
+++ Assets/Src/FollowRigidbody.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowRigidbody : MonoBehaviour {
+    public string rigidbodyName = "Rigid Body XXX";
+    private GameObject rigidBody;
+    private GroundMapping groundMapper;
+    private int checkCounter = 0;
+    public int checkInterval = 10;
+
+    void Awake()
+    {
+        GameObject ground = GameObject.Find("Ground");
+        if (ground == null)
+        {
+            Debug.LogError("There is no GameObject called Ground! Cannot map coordinates.");
+        }
+        else
+        {
+            groundMapper = GameObject.Find("Ground").GetComponent<GroundMapping>();
+        }
+    }
+
+	void Update () {
+        if (rigidBody == null && (checkCounter % checkInterval) == 0)
+        {
+            rigidBody = GameObject.Find(rigidbodyName);
+        }
+
+        if (rigidBody != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("X:" + rigidBody.transform.position.x + " Z:" + rigidBody.transform.position.z);
+            }
+
+            if(groundMapper != null) this.transform.position = groundMapper.translate(rigidBody.transform.position);
+        }
+	}
+}
---
+++ Assets/Src/GroundMapping.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Linear interpolation between coordinates supplied by OptiTrack and a Unity local quad.
+public class GroundMapping : MonoBehaviour {
+    // X scale of quad.
+    public int quadXSize = 10;
+    // Y scale of quad.
+    public int quadYSize = 10;
+
+    // X Value at leftmost point in room.
+    public float xMin = -1.4f;
+    // X Value at rightmost point in room.
+    public float xMax = 1.5f;
+
+    // Z Value furthest back in room.
+    public float zMin = 2.045f;
+    // Z Value furthest forward in room.
+    public float zMax = 4.4f;
+
+    // These values automatically calculated.
+    private float xMult;
+    private float zMult;
+
+    public GroundMapping()
+    {
+        xMult = quadXSize / (xMax - xMin);
+        zMult = quadYSize / (zMax - zMin);
+    }
+
+    // Translate from OptiTrack-provided coordinates
+    // to the local proportions of this ground quad.
+    public Vector3 translate(Vector3 input)
+    {
+        var x = (input.x - xMin) * xMult;
+        var z = (input.z - zMin) * zMult;
+        return new Vector3(x, 0f, z);
+    }
+}
